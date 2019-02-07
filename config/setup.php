7800c93721a41789410a99253cbdb8ab39393732
Harrison <harrison.selle@coloradocollege.edu>
2019-02-07 16:57:53 -0700

    removed login still kinda hacky

--- config/setup.php
+++ config/setup.php
@@ -37,13 +37,9 @@ $site_title = 'Course Proposal System';	//sets the site's title
 
 #Page Setup:
 if(!isset($path['call_parts'][0]) || $path['call_parts'][0] == '' ){
-	if($_SESSION['logged_in'] == true){
-		$page = 'home';
-		header('Location: home');	//sets blank/empty url to the home page
-	} else{
-		$page = 'home';
-		header('Location: login');	//sets blank/empty url to the dashboard
-	}
+	
+	$page = 'home';
+	header('Location: home');	//sets blank/empty url to the home page	
 } else{
 	$page = $path['call_parts'][0];
 }
