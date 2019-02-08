4ab04aebcc8c973b97b5ab4ce3f04902679e5cef
Harrison <harrison.selle@coloradocollege.edu>
2019-02-07 17:50:21 -0700

    Revert "fixing the results of my hacking" Reverting back to pre-hacking state This reverts commit 07a5dfd89ab7f04a4922f18c4e71985d2229c99d.

--- config/setup.php
+++ config/setup.php
@@ -37,6 +37,7 @@ $site_title = 'Course Proposal System';	//sets the site's title
 
 #Page Setup:
 if(!isset($path['call_parts'][0]) || $path['call_parts'][0] == '' ){
+	
 	$page = 'home';
 	header('Location: home');	//sets blank/empty url to the home page	
 } else{
@@ -46,9 +47,9 @@ if(!isset($path['call_parts'][0]) || $path['call_parts'][0] == '' ){
 
 
 #User Setup:
-// if($_SESSION['logged_in'] == true){
+if($_SESSION['logged_in'] == true){
 	$email = $_SESSION['user_email'];
 	$user = new User($dbc);
 	$user->fetchUserFromEmail($email);
-// }
+}
 ?>
