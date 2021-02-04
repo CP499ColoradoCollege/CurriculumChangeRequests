1c93d2c12b850cf7fd03ae41b872001c12c95798
case <cp_regan@coloradocollege.edu>
2021-02-04 14:52:06 -0700

    Restructuring, phase 2 (deleting old folders -- don't worry, no content was left behind it was just relocated)

--- config/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-#This page is responsible for connection to the MySQL Database associated with the project
-
-#Database Connection Here, sorted by host name, user name, user password, and then database name
-$dbc = mysqli_connect('localhost', 'root', '', 'proposal-toolDB') OR die('Error: '.mysqli_connect_error());
-?>
