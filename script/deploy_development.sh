162ad540d8e9ebd3362d44b085785a704636cfdd
lain <hbh137@protonmail.com>
2021-02-10 17:14:30 -0700

    Got more tests working. Got ExtractExcelData working.

--- script/deploy_development.sh
+++ script/deploy_development.sh
@@ -5,11 +5,13 @@ cd "$(dirname "$0")"
 echo "deploying to xampp..."
 echo "༼ つ ◕_◕ ༽つ"
 
+sudo cp -a tests vendor /opt/lampp/htdocs
+
 cd ../html
 
-sudo cp -a config classes databaseConnection functions tests vendor template views index.php .htaccess /opt/lampp/htdocs
+sudo cp -a config classes databaseConnection functions template views index.php .htaccess /opt/lampp/htdocs
 cd ../resources
-sudo cp banner.JPG  bootstrap.php composer.json /opt/lampp/htdocs
+sudo cp -a banner.JPG  bootstrap.php composer.json /opt/lampp/htdocs
 
 cd /opt/lampp/htdocs
 sudo composer install
--- tests/acceptance/HomeCest.php
+++ tests/acceptance/HomeCest.php
@@ -9,33 +9,33 @@ class HomeCest
 	/*
 	 * Tests that the home page opens when requested
 	 */
-    public function homepageWorks(AcceptanceTester $I)
-    {
-        $I->amOnPage('/home');
-        $I->see('My Proposals');  
-    }
+	public function homepageWorks(AcceptanceTester $I)
+   	{
+		$I->amOnPage('/home');
+		$I->see('My Proposals');  
+  	}
 	
 	/*
 	 * Tests that the New Proposal button on the Home page redirects to the correct page
 	 */
 	public function newProposalButtonWorks(AcceptanceTester $I)
-    {
-        $I->amOnPage('/home');
-        $I->see('My Proposals');  
+    	{
+		$I->amOnPage('/home');
+		$I->see('My Proposals');  
 		$I->click('New Proposal');
 		$I->canSeeInCurrentUrl('/new_proposal');
-    }
+    	}
 	
 	/*
 	 * Tests that the Edit button works for each Proposal on the Home page
 	 */
 	public function editProposalButtonWorks(AcceptanceTester $I)
-    {
-        $I->amOnPage('/home');
-        $I->see('Home');  
+    	{
+		$I->amOnPage('/home');
+		$I->see('Home');  
 		$I->click('Edit');
 		$I->canSeeInCurrentUrl('/edit_proposal');
-    }
+    	}
 	
 }
 
