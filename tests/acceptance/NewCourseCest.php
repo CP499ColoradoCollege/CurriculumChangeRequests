fc7c06fbb81fac22083b11afdf8d88317d65ce24
lain <hbh137@protonmail.com>
2021-02-13 17:27:25 -0700

    Finished tests.

--- tests/acceptance/NewCourseCest.php
+++ tests/acceptance/NewCourseCest.php
@@ -9,33 +9,33 @@ class NewCourseCest
 	/*
 	 * Tests that the user can navigate from the Home page to the Add New Course Proposal page
 	 */
-    public function newCourseProposalPageWorks(AcceptanceTester $I)
-    {
+	public function newCourseProposalPageWorks(AcceptanceTester $I)
+    	{
 		$I->amOnPage('/home');
-        $I->click('New Proposal');
-        $I->amOnPage('/new_proposal');
-        $I->see('New Proposal');  
+		$I->click('New Proposal');
+		$I->amOnPage('/new_proposal');
+		$I->see('New Proposal');  
 		$I->selectOption('type','1');
-		$I->click('begin');
-        $I->amOnPage('/new_course_proposal');
-        $I->see('Add New Course Proposal');  
-    }
+		$I->click('Begin Proposal');
+		$I->amOnPage('/new_course_proposal');
+		$I->see('Add New Course Proposal');  
+	}
 	
 	/*
 	 * Tests that the Add New Course Proposal form loads, that saving proposal correctly redirects the user,
 	 * and that the new proposal is added to user's Home page after saving the proposal
 	 */
 	public function newCourseProposalFormWorks(AcceptanceTester $I)
-    {
+    	{
 		$I->amOnPage('/home');
-        $I->click('New Proposal');
-        $I->amOnPage('/new_proposal');
-        $I->see('New Proposal');  
+		$I->click('New Proposal');
+		$I->amOnPage('/new_proposal');
+		$I->see('New Proposal');  
 		$I->selectOption('type','1');
-		$I->click('begin');
-        $I->amOnPage('/new_course_proposal');
-        $I->see('Add New Course Proposal');  
-		
+		$I->click('Begin Proposal');
+		$I->amOnPage('/new_course_proposal');
+		$I->see('Add New Course Proposal');  
+
 		$I->selectOption('department','Art');
 		$I->fillField('course_id', 'TE111');
 		$I->fillField('course_title', 'Test Course 1');
@@ -47,11 +47,11 @@ class NewCourseCest
 		$I->fillField('rationale', 'In order to test the site.');
 		$I->fillField('lib_impact', 'None');
 		$I->fillField('tech_impact', 'None.');
-		
+
 		$I->click('Save');
 		$I->canSeeInCurrentUrl('/home');
 		$I->see('New Course: TE111, Test Course 1');		
-    }
+    	}
 	
 	
 }
--- tests/acceptance/RemoveCourseCest.php
+++ tests/acceptance/RemoveCourseCest.php
@@ -9,33 +9,33 @@ class RemoveCourseCest
 	/*
 	 * Tests that the user can navigate from the Home page to the Remove Existing Course Proposal page
 	 */
-    public function removeCourseProposalPageWorks(AcceptanceTester $I)
-    {
-        $I->amOnPage('/home');
-        $I->click('New Proposal');
-        $I->amOnPage('/new_proposal');
-        $I->see('New Proposal');  
+	public function removeCourseProposalPageWorks(AcceptanceTester $I)
+    	{
+		$I->amOnPage('/home');
+		$I->click('New Proposal');
+		$I->amOnPage('/new_proposal');
+		$I->see('New Proposal');  
 		$I->selectOption('type','3');
-		$I->click('begin');
-        $I->amOnPage('/remove_course_proposal');
-        $I->see('Remove Existing Course Proposal');  
-    }
+		$I->click('Begin Proposal');
+		$I->amOnPage('/remove_course_proposal');
+		$I->see('Remove Existing Course Proposal');  
+    	}
 	
 	/*
 	 * Tests that the Remove Existing Course Proposal page/form load correctly, that the form redirects to the Home page once submitted,
 	 * and that the saved proposal is added to the user's Home page once saved
 	 */
 	public function removeCourseProposalFormWorks(AcceptanceTester $I)
-    {
-        $I->amOnPage('/home');
-        $I->click('New Proposal');
-        $I->amOnPage('/new_proposal');
-        $I->see('New Proposal');  
+    	{
+		$I->amOnPage('/home');
+		$I->click('New Proposal');
+		$I->amOnPage('/new_proposal');
+		$I->see('New Proposal');  
 		$I->selectOption('type','3');
-		$I->click('begin');
-        $I->amOnPage('/remove_course_proposal');
-        $I->see('Remove Existing Course Proposal');  
-		
+		$I->click('Begin Proposal');
+		$I->amOnPage('/remove_course_proposal');
+		$I->see('Remove Existing Course Proposal');  
+			
 		
 		$I->fillField('existing_course_id', 'CP122');
 		$I->fillField('rationale', 'In order to test the site once again.');
@@ -45,7 +45,7 @@ class RemoveCourseCest
 		$I->click('Save');
 		$I->canSeeInCurrentUrl('/home');
 		$I->see('Remove Course: CP122, Computer Science I');		
-    }
+    	}
 	
 	
 }
